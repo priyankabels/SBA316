53bea8124fcd8a207e223d775f01a08979337390
PriyankaRohit Belsare <priyankarohitbelsare@PriyankaRohits-MacBook-Pro.local>
2025-02-11 06:44:16 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,315 @@
+ 
+ //Declaring variables that neded to be cached with form values at the start of js
+ const taskForm=document.getElementById("formTaskManager");
+// console.log(taskForm)
+ const inputTextTask=document.querySelector("#inputTask");  // This is the place where user enteres text
+ const taskList=document.getElementById("taskList");  //Created Ul to store task
+ const taskListOptons=document.querySelector("#taskListOptons"); //Filter Completed and Incompleted tasks Form control
+ const addTaskButton=document.querySelector("#addTaskButton");
+
+// console.log(task);
+document.addEventListener("DOMContentLoaded", getTasksFromLocalStorage);
+
+function validateTaskInLocalStorage(taskText)
+{
+    let tasks=JSON.parse(localStorage.getItem("tasks"))||[];
+    if(tasks)
+    {
+        const sametask= tasks.find(task => task.taskText === taskText);
+        if(sametask)
+            return true;
+        else
+        return false;
+
+    }
+    else
+    return false;
+}
+addTaskButton.addEventListener("click",function(e)
+{
+    e.preventDefault();
+    console.log(inputTextTask.value)
+    const inputTask=inputTextTask.value.trim();
+    //Set custom validation return if null
+    if(inputTask==="")
+    {
+        window.alert("Input cannot be blank");
+        return;
+    }
+    //Validate to check if input task entered already matches with one in local storage then say it is duplicate reenter 
+    let repeatTask=validateTaskInLocalStorage(inputTextTask.value)
+    if(repeatTask)
+    {
+        window.alert("The task already exists. Reneter new task");
+        inputTextTask.value="";
+        return;
+    }
+    //Task creating using fragment 
+    createTask(inputTask,false,false)
+    inputTextTask.value="";
+    taskListOptons.value="all";
+    //inputTask=null;
+    // To get the parent of a task
+   const firstTask = taskList.firstChild;
+   
+});
+
+function createTask(inputTask,retrieval,completed)
+{
+    //console.log("Im called")
+    let index=0;
+    const frag=document.createDocumentFragment(); //Using Document fragment for better performance
+    //Creating a newTaskDiv and adding Li to hit to hold task created text
+    const newTaskDiv=document.createElement("div");
+    newTaskDiv.classList.add("task");
+    const taskLi=document.createElement("li")
+    taskLi.textContent=inputTask; //Li will contain the task that is created
+    //Adding tasks to local storage
+    if(retrieval===false)
+    {
+        saveTaskToLocalStorage(taskLi.textContent);  //Skip this while loading 
+    }
+   
+    taskLi.classList.add("task-item");
+    newTaskDiv.appendChild(taskLi);
+    
+
+   // Creating a button that can enable user mark task as completed
+    const taskCompletedBtn=document.createElement("button");
+    taskCompletedBtn.innerHTML="<i class='fa-solid fa-check-square'></li>"
+    taskCompletedBtn.classList.add("completeBtn");
+    if(completed===true)   //WHile retrieveing check if completed flag is true then mark show accordingly to UI 
+    {
+        newTaskDiv.classList.add("completed");
+        newTaskDiv.classList.add("task-text");
+    }
+    
+    newTaskDiv.appendChild(taskCompletedBtn);
+    //Creating a delete button that can enable user delete a task
+    const taskDeleteBtn=document.createElement("button");
+    taskDeleteBtn.innerHTML="<i class='fa-trash fa-solid'></li>"
+    taskDeleteBtn.classList.add("deleteBtn");
+    
+    newTaskDiv.appendChild(taskDeleteBtn);
+   //Appending the DIv created for a task to Frag
+    frag.appendChild(newTaskDiv);
+    taskList.appendChild(frag); //Appending fragment to taskList that is ul
+   // console.log(frag);
+
+}
+
+function saveTaskToLocalStorage(task)
+{
+    //localStorage.clear();
+    console.log(task);
+   let tasks;
+    console.log("savetolocal")
+    if(localStorage.getItem("tasks")===null)
+    {
+        tasks=[];  // set tasks to empty array if none are present
+        console.log("Empty task",tasks)
+    }
+    else
+    {
+        tasks=JSON.parse(localStorage.getItem("tasks")) //from local storage get tasks if any 
+        console.log("Existing task",tasks)
+    }
+    if(task!=="")
+    { 
+        const taskObj={
+            taskText:task,
+            completed:false
+        }
+        tasks.push(taskObj); //Push each task to tasks
+    }
+  
+    console.log("tasks before adding",tasks);
+
+    localStorage.setItem("tasks",JSON.stringify(tasks)); //Send tasks to local storage to save key here is tasks
+}
+//Whenever user deletes task from the list it should be deleted from local storage as well
+function deleteFromLocalStorage(task){
+    console.log(task.childNodes[0].textContent);
+    const taskNameToDelete = task.childNodes[0].textContent;
+    //Get tasks from local storage
+    let tasks=JSON.parse(localStorage.getItem("tasks"))||[];
+
+    // Find the index of the task that matches the taskName
+    const taskIndex = tasks.findIndex(task => task.taskText === taskNameToDelete);
+    console.log(taskIndex)
+    if (taskIndex >-1) {
+        // Remove the task at the found index
+        tasks.splice(taskIndex, 1);
+        
+        // Save the updated tasks array back to localStorage
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+    else
+    {
+        console.log("No tasks found to delete");
+    }
+
+}
+function getTasksFromLocalStorage()
+{
+    //localStorage.clear()
+    let tasks;
+    //JSON.parse(localStorage.getItem("tasks"))||[];
+    if(localStorage.getItem("tasks")===null)
+        {
+            tasks=[];  // set tasks to empty array if none are present
+            console.log("Empty task",tasks)
+        }
+        else
+        {
+            tasks=JSON.parse(localStorage.getItem("tasks")) //from local storage get tasks if any 
+            console.log("Existing task",tasks)
+        }
+    console.log("retrieve",tasks);
+
+    tasks.forEach(function(task)
+    {
+        console.log("Task retrieve",task.taskText);
+        createTask(task.taskText,true,task.completed);
+
+    });
+
+}
+taskList.addEventListener("click",function(e)
+{
+    e.preventDefault();
+    const task=e.target;
+    console.log("task",task);
+   try{
+    if(task.classList[0]==="completeBtn")   
+        {
+            console.log("in")
+            const taskItem=task.parentNode;
+            console.log("Completed button item",task.previousElementSibling.textContent)
+            task.classList.toggle("completed");
+            taskItem.classList.toggle("completed");
+            taskItem.classList.toggle("task-text");
+            //Update the local storage to mark flag as complted
+            if(task.previousElementSibling.textContent!=="")
+            {
+                console.log("Update called");
+                const isCompleted=taskItem.classList.contains("completed");
+              updateTaskCompleteStatus(task.previousElementSibling.textContent,isCompleted)
+            }
+        }
+        if(task.classList[0]==="deleteBtn")
+        {
+            const taskItem=task.parentNode;
+            //Remove from Local Storage so passing full taskItem to get the text 
+            console.log("Calling delete",taskItem);
+            deleteFromLocalStorage(taskItem);
+            taskItem.remove();
+        }
+
+   }
+    catch
+    {
+        throw new Error("Error caused while deleting the task or maeking it complete");
+        
+    }
+    
+});
+//Add an event listner to the filter box
+taskListOptons.addEventListener("change",function(e)
+{
+    const tasks=taskList.childNodes;
+    console.log(tasks);
+    tasks.forEach(function(task){
+        switch(e.target.value)
+        {
+            case "all":
+                task.style.display="flex";
+                break;
+            case "completed":
+                if(task.classList.contains("completed"))
+                {
+                    task.style.display="flex";
+                }
+                else
+                {
+                    task.style.display="none";
+                }
+                break;
+            case "incomplete":
+                if(!task.classList.contains("completed"))  //Means it is incomplete then display else not 
+                {
+                    task.style.display="flex";
+                }
+                else
+                {
+                task.style.display="none";
+                }
+                break;
+        }
+    });
+});
+
+function filterTasks(e){
+    const tasks=taskList.childNodes;
+    tasks.forEach(function(task){
+        switch(e.target.value)
+        {
+            case "all":
+                task.style.display="flex";
+                break;
+            case "completed":
+                if(task.classList.contains("completed"))
+                {
+                    task.style.display="flex";
+                }
+                else
+                {
+                    task.style.display="none";
+                }
+                break;
+            case "incomplete":
+                if(!task.classList.contains("complete"))  //Means it is incomplete then display else not 
+                {
+                    task.style.display="flex";
+                }
+                else
+                {
+                task.style.display="none";
+                }
+                break;
+        }
+    });
+
+}
+
+function updateTaskCompleteStatus(taskText,completed)
+{
+    // Retrieve the tasks from localStorage
+    console.log(taskText);
+    console.log(completed)
+    let tasks = JSON.parse(localStorage.getItem("tasks"));
+    
+    if (tasks === null) {
+        console.log("No tasks found in localStorage.");
+        return;
+    }
+    try{
+            // Find the task by its name 
+        const taskToUpdate = tasks.find(task => task.taskText === taskText);
+        
+        if (taskToUpdate) {
+            // Update the completed of the found task
+            taskToUpdate.completed = completed;
+            
+            // Save the updated tasks array back to localStorage
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+        }
+
+    }
+    catch{
+        throw new Error("Error while updating the completed property of task ")
+    }
+    
+    
+}
+
